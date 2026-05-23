e3e6cc038de8392456a168b8e454d49788be70b4
Andrei Preda <andrei.preda0308@stud.acs.upb.ro>
2026-05-23 13:17:20 +0300

    Implement random seed

--- src/main.c
+++ src/main.c
@@ -12,11 +12,28 @@
 #define DC_PIN 9
 #define CS_PIN 10
 
+static void random_seed_init() {
+    uint8_t old_admux = ADMUX;
+    uint8_t old_adcsra = ADCSRA;
+
+    ADMUX = (1 << REFS0) | (1 << MUX2) | (1 << MUX0);
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+    ADCSRA |= (1 << ADSC);
+    while (ADCSRA & (1 << ADSC));
+
+    srand(ADC);
+
+    ADMUX = old_admux;
+    ADCSRA = old_adcsra;
+}
+
 int main() {
     joystick_init();
     systime_init();
     rgb_led_init();
     buzzer_init();
+    random_seed_init();
 
     ili9341_240x320_spi_init(RESET_PIN, CS_PIN, DC_PIN);
     ili9341_setRotation(3);
